bc065af2a8405a2999e0c0dd9fecd2a7a7eab544
d4wass <dwasilewski.93@gmail.com>
2021-03-29 21:41:38 +0200

    add sounds and events to btn for playing sounds

--- lab2/index.js
+++ lab2/index.js
@@ -1,5 +1,5 @@
-var SoundPanel = /** @class */ (function () {
-    function SoundPanel() {
-    }
-    return SoundPanel;
-}());
+var pads = document.querySelectorAll('.pad');
+var audio = document.getElementsByTagName('audio');
+var playAudio = function (audio) { return (audio.play()); };
+var signAudioToBtn = function (pads, audio) { return (pads.forEach(function (pad, index) { return pad.addEventListener('click', function () { return audio[index].play(); }); })); };
+signAudioToBtn(pads, audio);
--- lab2/index.ts
+++ lab2/index.ts
@@ -1,6 +1,14 @@
-class SoundPanel {
-    soundPads: NodeListOf<HTMLElement>;
-    sounds: NodeListOf<HTMLAudioElement>;
-    
+const pads: NodeListOf<Element> = document.querySelectorAll('.pad');
+const audio: HTMLCollectionOf<HTMLAudioElement> = document.getElementsByTagName('audio');
 
-}
+const playAudio: Function = (audio: HTMLAudioElement) => (
+    audio.play()
+);
+
+const signAudioToBtn = (pads: NodeListOf<Element>, audio: HTMLCollectionOf<HTMLAudioElement>) => (
+    pads.forEach(
+        (pad: Element, index: number) => pad.addEventListener('click', () => audio[index].play())
+    )
+);
+
+signAudioToBtn(pads, audio);
