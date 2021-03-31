fd8fee3c512071329209c6bc740f2946056b5a12
d4wass <dwasilewski.93@gmail.com>
2021-03-31 13:59:23 +0200

    adding one record channel

--- lab2/index.js
+++ lab2/index.js
@@ -1,5 +1,39 @@
-var pads = document.querySelectorAll('.pad');
-var audio = document.getElementsByTagName('audio');
-var playAudio = function (audio) { return (audio.play()); };
-var signAudioToBtn = function (pads, audio) { return (pads.forEach(function (pad, index) { return pad.addEventListener('click', function () { return audio[index].play(); }); })); };
-signAudioToBtn(pads, audio);
+let pads;
+let audio;
+let btnChannelsRec;
+let btnChannelsPlay;
+const channel1Play = [];
+const appStart = () => {
+    pads = document.querySelectorAll('.pad');
+    audio = document.getElementsByTagName('audio');
+    btnChannelsPlay = document.querySelectorAll('[data-channel-play]');
+    btnChannelsRec = document.querySelectorAll('[data-channel-rec]');
+    document.addEventListener('keypress', onKeyPress);
+    console.log(pads);
+    console.log(btnChannelsRec);
+};
+const onKeyPlaySound = (index) => {
+    audio[index].currentTime = 0;
+    audio[index].play();
+};
+const playRecordedSound = (key) => {
+    let arrPads = Array.from(pads);
+    let indexSound = arrPads.findIndex(element => element.dataset.key === key);
+    onKeyPlaySound(indexSound);
+};
+const playChannelRecord = () => {
+    channel1Play.forEach(sound => {
+        setTimeout(() => playRecordedSound(sound.key), sound.time);
+    });
+};
+const onKeyPress = (e) => {
+    pads.forEach((pad, index) => {
+        if (e.key === pad.dataset.key) {
+            onKeyPlaySound(index);
+            channel1Play.push({ key: e.key, time: e.timeStamp });
+            console.log(channel1Play);
+        }
+    });
+};
+appStart();
+btnChannelsPlay[0].addEventListener('click', () => playChannelRecord());
--- lab2/index.ts
+++ lab2/index.ts
@@ -1,14 +1,49 @@
-const pads: NodeListOf<Element> = document.querySelectorAll('.pad');
-const audio: HTMLCollectionOf<HTMLAudioElement> = document.getElementsByTagName('audio');
+let pads: NodeListOf<HTMLElement>;
+let audio: HTMLCollectionOf<HTMLAudioElement>;
+let btnChannelsRec: NodeListOf<HTMLElement>;
+let btnChannelsPlay: NodeListOf<HTMLElement>;
 
-const playAudio: Function = (audio: HTMLAudioElement) => (
-    audio.play()
-);
+const channel1Play: any[] = []
 
-const signAudioToBtn = (pads: NodeListOf<Element>, audio: HTMLCollectionOf<HTMLAudioElement>) => (
-    pads.forEach(
-        (pad: Element, index: number) => pad.addEventListener('click', () => audio[index].play())
-    )
-);
+const appStart = (): void => {
+    pads = document.querySelectorAll('.pad');
+    audio = document.getElementsByTagName('audio');
+    btnChannelsPlay = document.querySelectorAll('[data-channel-play]');
+    btnChannelsRec = document.querySelectorAll('[data-channel-rec]');
 
-signAudioToBtn(pads, audio);
+    document.addEventListener('keypress', onKeyPress);
+    console.log(pads);
+    console.log(btnChannelsRec);
+}
+
+const onKeyPlaySound = (index: number): void => {
+    audio[index].currentTime = 0;
+    audio[index].play();
+}
+
+const playRecordedSound = (key: string): void => {
+    let arrPads = Array.from(pads)
+    let indexSound = arrPads.findIndex(element => element.dataset.key === key)
+    
+    onKeyPlaySound(indexSound)
+}
+
+const playChannelRecord = () => {
+    channel1Play.forEach(sound => {
+        setTimeout(() => playRecordedSound(sound.key), sound.time)
+    })
+}
+
+
+const onKeyPress = (e: KeyboardEvent): void => { 
+    pads.forEach((pad: HTMLElement, index: number) => {
+        if (e.key === pad.dataset.key) {
+            onKeyPlaySound(index)
+            channel1Play.push({ key: e.key, time: e.timeStamp })
+            console.log(channel1Play)
+        }
+    })
+}
+
+appStart();
+btnChannelsPlay[0].addEventListener('click', () => playChannelRecord())
