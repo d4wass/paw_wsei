83fee2512de476dd57af76575850b0a10b5c80d8
d4wass <dwasilewski.93@gmail.com>
2021-04-09 18:06:32 +0200

    adding delete btn to channel

--- lab2/index.js
+++ lab2/index.js
@@ -2,6 +2,7 @@ let pads;
 let audio;
 let btnChannelsRec;
 let btnChannelsPlay;
+let btnChannelsDelete;
 let selectedChannel;
 const channel1 = [];
 const channel2 = [];
@@ -12,22 +13,25 @@ const appStart = () => {
     audio = document.getElementsByTagName('audio');
     btnChannelsPlay = document.querySelectorAll('[data-channel-play]');
     btnChannelsRec = document.querySelectorAll('[data-channel-rec]');
+    btnChannelsDelete = document.querySelectorAll('[data-channel-del]');
     document.addEventListener('keypress', onKeyPress);
     btnChannelsPlay.forEach(btnPlay => btnPlay.addEventListener('click', (e) => playChannelRecord(e)));
     btnChannelsRec.forEach(btnRec => btnRec.addEventListener('click', () => recOnChannel(btnRec)));
+    btnChannelsDelete.forEach(btnDel => btnDel.addEventListener('click', () => deleteFromChannel(btnDel)));
 };
-const onKeyPlaySound = (index) => {
-    audio[index].currentTime = 0;
+const onKeyPlaySound = (index, currentTime = 0) => {
+    console.log(currentTime);
+    audio[index].currentTime = currentTime;
     audio[index].play();
 };
-const playRecordedSound = (key) => {
+const playRecordedSound = (key, time) => {
     let arrPads = Array.from(pads);
     let indexSound = arrPads.findIndex(element => element.dataset.key === key);
-    onKeyPlaySound(indexSound);
+    onKeyPlaySound(indexSound, time);
 };
 const playFromChannel = (channel) => {
     channel.forEach(sound => {
-        setTimeout(() => playRecordedSound(sound.key), sound.timeStamp);
+        setTimeout(() => playRecordedSound(sound.key, sound.timeStamp), sound.timeStamp);
     });
 };
 const playChannelRecord = ({ target: { dataset } }) => {
@@ -56,6 +60,20 @@ const recOnChannel = (btnRec) => {
     }
     if (btnRec.dataset.channelRec === "4") {
         selectedChannel = channel4;
+    }
+};
+const deleteFromChannel = (btnDel) => {
+    if (btnDel.dataset.channelDel === "1") {
+        channel1.length = 0;
+    }
+    if (btnDel.dataset.channelDel === "2") {
+        channel2.length = 0;
+    }
+    if (btnDel.dataset.channelDel === "3") {
+        channel3.length = 0;
+    }
+    if (btnDel.dataset.channelDel === "4") {
+        channel4.length = 0;
     }
 };
 const onKeyPress = ({ key, timeStamp }) => {
--- lab2/index.ts
+++ lab2/index.ts
@@ -2,6 +2,7 @@ let pads: NodeListOf<HTMLElement>;
 let audio: HTMLCollectionOf<HTMLAudioElement>;
 let btnChannelsRec: NodeListOf<HTMLElement>;
 let btnChannelsPlay: NodeListOf<HTMLElement>;
+let btnChannelsDelete: NodeListOf<HTMLElement>;
 let selectedChannel: Array<any>
 
 const channel1: Array<{ key: string, timeStamp: number }> = [];
@@ -14,32 +15,34 @@ const appStart = (): void => {
     audio = document.getElementsByTagName('audio');
     btnChannelsPlay = document.querySelectorAll('[data-channel-play]');
     btnChannelsRec = document.querySelectorAll('[data-channel-rec]');
+    btnChannelsDelete = document.querySelectorAll('[data-channel-del]');
 
     document.addEventListener('keypress', onKeyPress);
     btnChannelsPlay.forEach(btnPlay => btnPlay.addEventListener('click', (e) => playChannelRecord(e)))
     btnChannelsRec.forEach(btnRec => btnRec.addEventListener('click', () => recOnChannel(btnRec)))
+    btnChannelsDelete.forEach(btnDel => btnDel.addEventListener('click', () => deleteFromChannel(btnDel)))
 }
 
-
-const onKeyPlaySound = (index: number): void => {
-    audio[index].currentTime = 0;
+const onKeyPlaySound = (index: number, currentTime: number = 0): void => {
+    console.log(currentTime)
+    audio[index].currentTime = currentTime;
     audio[index].play();
 }
 
-const playRecordedSound = (key: string): void => {
+const playRecordedSound = (key: string, time: number): void => {
     let arrPads = Array.from(pads)
     let indexSound = arrPads.findIndex(element => element.dataset.key === key)
-
-    onKeyPlaySound(indexSound)
+    onKeyPlaySound(indexSound, time)
 }
 
 const playFromChannel = (channel: Array<{ key: string, timeStamp: number }>): void => {
     channel.forEach(sound => {
-        setTimeout(() => playRecordedSound(sound.key), sound.timeStamp)
+        setTimeout(() => playRecordedSound(sound.key, sound.timeStamp), sound.timeStamp)
     })
 }
+
 const playChannelRecord = ({ target: { dataset }}: any): void => {
-     if (dataset.channelPlay === "1") {
+    if (dataset.channelPlay === "1") {
         playFromChannel(channel1)
     }
     if (dataset.channelPlay === "2") {
@@ -51,7 +54,6 @@ const playChannelRecord = ({ target: { dataset }}: any): void => {
     if (dataset.channelPlay === "4") {
         playFromChannel(channel4)
     }
-
 }
 
 const recOnChannel = (btnRec?: HTMLElement): void => {
@@ -67,7 +69,21 @@ const recOnChannel = (btnRec?: HTMLElement): void => {
     if (btnRec.dataset.channelRec === "4") {
         selectedChannel = channel4
     }
+}
 
+const deleteFromChannel = (btnDel?: HTMLElement): void => {
+    if (btnDel.dataset.channelDel === "1") {
+        channel1.length = 0
+    }
+    if (btnDel.dataset.channelDel === "2") {
+        channel2.length = 0
+    }
+    if (btnDel.dataset.channelDel === "3") {
+        channel3.length = 0
+    }
+    if (btnDel.dataset.channelDel === "4") {
+        channel4.length = 0
+    }
 }
 
 
